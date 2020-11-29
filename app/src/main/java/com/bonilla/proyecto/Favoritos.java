dc44ab53f260e86c0d078fdfddc243edad4cc3b9
cristhian0314 <57076618+cristhian0314@users.noreply.github.com>
2020-11-29 10:12:39 -0500

    Interfaces restantes

---
+++ app/src/main/java/com/bonilla/proyecto/Favoritos.java
@@ -0,0 +1,13 @@
+package com.bonilla.proyecto;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Favoritos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favoritos);
+    }
+}
---
+++ app/src/main/java/com/bonilla/proyecto/Recomendados.java
@@ -0,0 +1,13 @@
+package com.bonilla.proyecto;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Recomendados extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recomendados);
+    }
+}
