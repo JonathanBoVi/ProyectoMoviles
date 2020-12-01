4ab10fb8ef378994a3e9bf7a1933d3d1823d1541
JonathanBoVi <jonathanbovi99@gmail.com>
2020-12-01 17:01:48 -0500

    holi2

--- app/src/main/java/com/bonilla/proyecto/MenuPrincipal.java
+++ app/src/main/java/com/bonilla/proyecto/MenuPrincipal.java
@@ -9,12 +9,15 @@ import android.widget.Button;
 
 public class MenuPrincipal extends AppCompatActivity {
 
-    Button btnConfiguracion;
+    Button btnConfiguracion,btnFavoritos,btnRecomendados,btnBuscar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_principal);
         btnConfiguracion=findViewById(R.id.btnConfiguracion);
+        btnFavoritos=findViewById(R.id.btnFavoritos);
+        btnRecomendados=findViewById(R.id.btnRecomendado);
+        btnBuscar=findViewById(R.id.btnBuscarHotel);
 
         btnConfiguracion.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -23,7 +26,31 @@ public class MenuPrincipal extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-    }
+
+        btnBuscar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getApplicationContext(),Mapa.class);
+                startActivity(intent);
+            }
+        });
+
+        btnFavoritos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getApplicationContext(),Favoritos.class);
+                startActivity(intent);
+            }
+        });
+
+        btnRecomendados.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getApplicationContext(),Recomendados.class);
+                startActivity(intent);
+            }
+        });
+}
 
 
 
