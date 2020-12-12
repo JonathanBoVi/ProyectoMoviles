0a1960d21611555507ee479b980356c4165b6376
cristhian0314 <57076618+cristhian0314@users.noreply.github.com>
2020-12-11 22:23:34 -0500

    foto

--- app/src/main/java/com/bonilla/proyecto/Configuracion.java
+++ app/src/main/java/com/bonilla/proyecto/Configuracion.java
@@ -2,30 +2,53 @@ package com.bonilla.proyecto;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.Parcelable;
 import android.provider.MediaStore;
+import android.util.Base64;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.firebase.auth.FirebaseAuth;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Hashtable;
 import java.util.List;
+import java.util.Map;
 
 public class Configuracion extends AppCompatActivity {
 
     Button btnCerrar;
     Button btnInicio,btnFavoritos,btnRecomendados,btnFoto;
+    private Bitmap bitmap;
+    private ImageView imgFoto;
+    private String KEY_IMAGEN = "foto";
 
     private View.OnClickListener lisFot=new View.OnClickListener(){
         @Override
@@ -85,6 +108,83 @@ public class Configuracion extends AppCompatActivity {
 
     }
 
+    private void uploadImage() {
+        //Mostrar el diálogo de progreso
+        final ProgressDialog loading = ProgressDialog.show(this, "Subiendo...", "Espere por favor...", false, false);
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, "https://proyectofinalhotel.000webhostapp.com/subirFoto.php",
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String s) {
+                        //Descartar el diálogo de progreso
+                        loading.dismiss();
+                        //Mostrando el mensaje de la respuesta
+                        Toast.makeText(Configuracion.this, s, Toast.LENGTH_LONG).show();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError volleyError) {
+                        //Descartar el diálogo de progreso
+                        loading.dismiss();
+
+                        //Showing toast
+                        Toast.makeText(Configuracion.this, volleyError.getMessage().toString(), Toast.LENGTH_LONG).show();
+                    }
+                }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                //Convertir bits a cadena
+                String imagen = getStringImagen(bitmap);
+
+                //Creación de parámetros
+                Map<String, String> params = new Hashtable<String, String>();
+
+                //Agregando de parámetros
+                params.put(KEY_IMAGEN, imagen);
+
+                //Parámetros de retorno
+                return params;
+            }
+        };
+
+        //Creación de una cola de solicitudes
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+
+        //Agregar solicitud a la cola
+        requestQueue.add(stringRequest);
+    }
+
+    public String getStringImagen(Bitmap bmp){
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bmp.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        byte[] imageBytes = baos.toByteArray();
+        String encodedImage = Base64.encodeToString(imageBytes, Base64.DEFAULT);
+        return encodedImage;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 1 && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            Uri filePath = data.getData();
+            try {
+                //Cómo obtener el mapa de bits de la Galería
+                Log.e("Error1", "xd");
+                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
+
+                //Configuración del mapa de bits en ImageView
+                Log.e("Error2", "xd");
+                uploadImage();
+                //imgFoto.setImageBitmap(bitmap);
+                Log.e("Error3", "xd");
+            } catch (IOException e) {
+                Log.e("ERROR REAL", e.getMessage());
+                e.printStackTrace();
+            }
+        }
+    }
+
     private void tomarFoto(){
         // Determine Uri of camera image to save.
         final File root = new File(Environment.getExternalStorageDirectory() + File.separator + "MyDir" + File.separator);
@@ -120,15 +220,5 @@ public class Configuracion extends AppCompatActivity {
 
         startActivityForResult(chooserIntent, 1);
     }
-
-    private ArrayList<Hotel> GetArrayItem(){
-        ArrayList<Hotel> listaItem = new ArrayList<>();
-        listaItem.add(new Hotel(R.drawable.casandina, "Casa Andina Select", "Federico Villarreal 115"));
-        listaItem.add(new Hotel(R.drawable.sunec, "Sunec Hotel", "Manuel María Izaga 472"));
-        listaItem.add(new Hotel(R.drawable.tumbasreales, "Tumbas Reales", "calle Andalucia 198 - 208 urb"));
-
-        return listaItem;
-    }
-
 
 }
