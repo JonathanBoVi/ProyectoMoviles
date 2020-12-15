daa2d8543827f6358c2b43b4afb24bfb1da6812f
cristhian0314 <57076618+cristhian0314@users.noreply.github.com>
2020-12-15 00:19:55 -0500

    listar recomendados

--- app/src/main/java/com/bonilla/proyecto/AdaptadorRecomendado.java
+++ app/src/main/java/com/bonilla/proyecto/AdaptadorRecomendado.java
@@ -1,13 +1,21 @@
 package com.bonilla.proyecto;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 
 public class AdaptadorRecomendado extends BaseAdapter{
@@ -42,10 +50,47 @@ public class AdaptadorRecomendado extends BaseAdapter{
         ImageView img = (ImageView) convertView.findViewById(R.id.imgHotel2);
         TextView txtHotel = (TextView) convertView.findViewById((R.id.txtNombreHotel2));
         TextView txtDescripcion =  (TextView) convertView.findViewById(R.id.txtDescripcionHotel2);
+        RatingBar ratingBar = (RatingBar) convertView.findViewById(R.id.txtEstrellas);
 
-        img.setImageResource(item.getImg());
+        new GetImageToURL(img).execute("https://proyectofinalhotel.000webhostapp.com/uploads/"+item.getNombreHotel()+".png");
+
+        //img.setImageResource(item.getImg());
         txtHotel.setText(item.getNombreHotel());
         txtDescripcion.setText(item.getDescripcionHotel());
+        ratingBar.setRating(item.getCalificacion());
+
         return convertView;
+    }
+
+    private class GetImageToURL extends AsyncTask< String, Void, Bitmap> {
+        private ImageView img;
+
+        public GetImageToURL(ImageView img) {
+            this.img = img;
+        }
+
+        @Override
+        protected Bitmap doInBackground(String...params) {
+            try {
+                URL url = new URL(params[0]);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setDoInput(true);
+                connection.connect();
+                InputStream input = connection.getInputStream();
+                Bitmap myBitmap = BitmapFactory.decodeStream(input);
+                return myBitmap;
+            } catch (IOException e) {
+                // Log exception
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap myBitMap) {
+            if (myBitMap!=null){
+                img.setImageBitmap(myBitMap);
+            }
+
+        }
     }
 }
--- app/src/main/java/com/bonilla/proyecto/Recomendados.java
+++ app/src/main/java/com/bonilla/proyecto/Recomendados.java
@@ -4,11 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Recomendados extends AppCompatActivity {
 
@@ -16,6 +32,10 @@ public class Recomendados extends AppCompatActivity {
 
     private ListView lista;
     private AdaptadorRecomendado adaptador2;
+
+    String nombre, descripcion;
+    int calificacion;
+    private ArrayList<Hotel> listaItem = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -49,17 +69,69 @@ public class Recomendados extends AppCompatActivity {
         });
 
         lista = (ListView) findViewById(R.id.lblItem2);
-        adaptador2 = new AdaptadorRecomendado(this, GetArrayItem());
-        lista.setAdapter(adaptador2);
+        listarRecomendados("http://proyectofinalhotel.000webhostapp.com/listarRecomendados.php");
+        /*adaptador2 = new AdaptadorRecomendado(this, GetArrayItem());
+        lista.setAdapter(adaptador2);*/
 
     }
 
-    private ArrayList<Hotel> GetArrayItem(){
+    private void listarRecomendados(String URL){
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>(){
+
+            @Override
+            public void onResponse(String response) {
+                if (!response.isEmpty()) {
+                    try {
+
+                        JSONArray jsonArr = new JSONArray(response);
+                        for (int i = 0; i < jsonArr.length(); i++) {
+                            JSONObject objeto = jsonArr.getJSONObject(i);
+                            nombre = objeto.getString("nombre");
+                            descripcion = objeto.getString("descripcion");
+                            calificacion = objeto.getInt("calificacion");
+                            Hotel hotelito = new Hotel(R.drawable.winmeier, nombre, descripcion,calificacion);
+                            listaItem.add(hotelito);
+
+
+                            Log.e("response",listaItem.get(0).toString());
+
+
+                        }
+
+                        adaptador2 = new AdaptadorRecomendado(getApplicationContext(), listaItem);
+                        lista.setAdapter(adaptador2);
+
+                    } catch (JSONException e) {
+                        Log.e("respo",e.getMessage());
+                        e.printStackTrace();
+                    }
+                }else {
+                    Toast.makeText(Recomendados.this, "Error en el response", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(Recomendados.this, "Error en el lsitener", Toast.LENGTH_SHORT).show();
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> parametros=new HashMap<String,String>();
+                parametros.put("usu",String.valueOf(Utilidades.getCorreo()));
+                return parametros;
+            }
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }
+
+    /*private ArrayList<Hotel> GetArrayItem(){
         ArrayList<Hotel> listaItem = new ArrayList<>();
         listaItem.add(new Hotel(R.drawable.casandina, "Casa Andina Select", "Federico Villarreal 115"));
         listaItem.add(new Hotel(R.drawable.sunec, "Sunec Hotel", "Manuel María Izaga 472"));
         listaItem.add(new Hotel(R.drawable.tumbasreales, "Tumbas Reales", "calle Andalucia 198 - 208 urb"));
         listaItem.add(new Hotel(R.drawable.winmeier, "Win Meier", "Av. Francisco Bolognesi 756"));
         return listaItem;
-    }
+    }*/
 }
